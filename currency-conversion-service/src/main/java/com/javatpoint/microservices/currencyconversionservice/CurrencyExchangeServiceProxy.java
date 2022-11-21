bcc9e94938a8bb936eb24fe6ba883edc2f03f2b8
Tam Nguyen <tam.nguyen@bkitsolution.com>
2022-11-21 11:22:07 +0700

    Zuul

--- currency-conversion-service/src/main/java/com/javatpoint/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/javatpoint/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -4,11 +4,18 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+
 //@FeignClient(name="currency-exchange-service", url="http://localhost:8000")
+
+
 //Enabling feign
 @FeignClient(name="currency-exchange-service")
 //enabling ribbon
 @RibbonClient(name="currency-exchange-service")
+
+// Zuul
+//@FeignClient(name="netflix-zuul-api-gateway-server")
 public interface CurrencyExchangeServiceProxy
 {
   @GetMapping("/currency-exchange/from/{from}/to/{to}")		//where {from} and {to} are path variable
